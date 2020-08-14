cfcabb819ba1009c5c5f1246ee5c96be058facc5
jurgen1c <jurgen1c@gmail.com>
2020-08-14 15:24:05 -0600

    Setup linters & basic file structure

---
+++ enumerables.rb
@@ -0,0 +1,45 @@
+module Enumerable
+
+  def my_each
+
+    yield
+
+  end
+
+  def my_each_with_index
+    yield
+  end
+
+  def my_select
+    yield
+  end
+
+  def my_all?
+    yield
+  end
+
+  def my_any?
+    yield
+  end
+
+  def my_none?
+    yield
+  end
+
+  def my_count
+    yield
+  end
+
+  def my_map
+    yield
+  end
+
+  def my_inject
+    yield
+  end
+
+  def multiply_ls
+    yield
+  end
+
+end
