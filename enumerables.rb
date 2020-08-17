8dbe1118364097da8319cd267ce9b22009254fff
hguruman <hguruman@gmail.com>
2020-08-17 20:55:26 +0400

    Fix bugs: Update linters errors detected by rubocop

--- enumerables.rb
+++ enumerables.rb
@@ -1,7 +1,5 @@
 module Enumerable
-
   # My Each Method
-
   def my_each
     return to_enum(:my_each) unless block_given?
     arr = self.class == Array ? self : to_a
@@ -12,9 +10,7 @@ module Enumerable
     end
     arr
   end
-
   # My Each With Index
-
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
     arr = self.class == Array ? self : to_a
@@ -25,20 +21,16 @@ module Enumerable
     end
     arr
   end
-
   # My Select
-
   def my_select
     new_array = []
-    my_each() {|index_value| new_array << index_value if yield(index_value)}
+    my_each {|index_value| new_array << index_value if yield(index_value)}
     puts new_array
   end
-
   # My All?
-
   def my_all?
     result = nil
-    self.my_each do | item |
+    my_each do |item|
       if yield(item)
         result = true
       else
@@ -46,12 +38,10 @@ module Enumerable
     end
     result
   end
-
   # My Any?
-
   def my_any?
     result = false
-    self.my_each do | item |
+    my_each do |item|
       next unless yield(item)
         result = true
       end
@@ -59,12 +49,10 @@ module Enumerable
     end
     result
   end
-
   # My None?
-
   def my_none?
     result = false
-    self.my_each do | item |
+    my_each do |item|
       if yield(item)
         result = false
       else
@@ -74,30 +62,23 @@ module Enumerable
     end
     result
   end
-
   # My Count
-
   def my_count
     counter = 0
-    self.my_each do | item |
+    my_each do |item|
       counter += 1 if yield(item)
     end
     counter
   end
-
-
   # My Map 
-
   def my_map(&block_given)
     result = []
-    self.my_each do |element|
+    my_each do |element|
       result << block_given.call(element)
     end
     result
   end
-
   # My Inject
-
   def my_inject(start_value = 0)
     counter = 0
     accumulator = start_value
@@ -107,11 +88,9 @@ module Enumerable
     end
     accumulator
   end
-
   # Multiply LS
-
   def multiply_els
-    self.my_inject(1){| a, b | a * b }
+    self.my_inject(1){|a, b|a * b}
   end
 
   my_arr = [34, 1, 25, 91, 6]
@@ -132,22 +111,22 @@ module Enumerable
   end
 
   puts 'my_all?'
-  puts my_arr.my_all?{ | n | n == 6 }
+  puts my_arr.my_all?{ |n| n == 6 }
 
   puts 'my_any?'
-  puts my_arr.my_any?{ | n | n == 1 }
+  puts my_arr.my_any?{ |n| n == 1 }
 
   puts 'my_none?'
-  puts my_arr.my_none?{ | n | n > 6 }
+  puts my_arr.my_none?{ |n| n > 6 }
 
   puts 'my-count'
-  puts my_arr.my_count{ | n | n % 2 == 0 }
+  puts my_arr.my_count{ |n| n % 2 == 0 }
 
   puts 'my_map'
-  puts my_arr.my_map{ | n | n + 4 }
+  puts my_arr.my_map{ |n| n + 4 }
 
   puts 'my_inject'
-  puts my_arr.my_inject(3){ | n, y | n + y}
+  puts my_arr.my_inject(3){ |n, y| n + y}
 
   puts 'multiply_els'
   puts my_arr.multiply_els()
