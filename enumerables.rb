ccf05378072431d509152ae32f2fbc1b44f1b42d
jurgen1c <jurgen1c@gmail.com>
2020-08-17 13:47:24 -0600

    Fix spacing, indentation & formatting of script

--- enumerables.rb
+++ enumerables.rb
@@ -28,7 +28,7 @@ module Enumerable
   # My Select
   def my_select
     new_array = []
-    my_each do |index_value| 
+    my_each do |index_value|
       new_array << index_value if yield(index_value)
     end
     new_array
@@ -67,13 +67,13 @@ module Enumerable
   # My Count
   def my_count
     counter = 0
-    my_each do |item| 
+    my_each do |item|
       counter += 1 if yield(item)
     end
     counter
   end
 
-  # My Map 
+  # My Map
   def my_map(&block_given)
     result = []
     my_each do |element|
@@ -87,7 +87,7 @@ module Enumerable
     counter = 0
     accumulator = start_value
     while counter < self.size
-      accumulator =  yield(accumulator, self[counter])
+      accumulator = yield(accumulator, self[counter])
       counter += 1
     end
     accumulator
@@ -95,7 +95,7 @@ module Enumerable
 
   # Multiply LS
   def multiply_els
-    self.my_inject(1){ |a, b|a * b }
+    self.my_inject(1) { |a, b|a * b }
   end
 end
 
@@ -103,33 +103,33 @@ my_arr = [34, 1, 25, 91, 6]
 
 puts 'my_each Array'
 [1, 2, 34, 44, 54].my_each do |n|
-  puts n 
-  end
+    puts n
+end
 
 puts 'my_each_with_index Array'
 [55, 6, 89, 74, 57].my_each_with_index do |n, i|
-  puts i.to_s + ' : ' + n.to_s
-  end
+    puts i.to_s + ' : ' + n.to_s
+end
 
 puts 'my_select'
-puts [8, 5, 69, 10, 7].my_select { |n| n > 6 }
+puts [8, 5, 69, 10, 7].my_select() { |n| n > 6 }
 
 puts 'my_all?'
-puts my_arr.my_all?{ |n| n == 6 }
+puts my_arr.my_all?() { |n| n == 6 }
 
 puts 'my_any?'
-puts my_arr.my_any?{ |n| n == 1 }
+puts my_arr.my_any?() { |n| n == 1 }
 
 puts 'my_none?'
-puts my_arr.my_none?{ |n| n > 6 }
+puts my_arr.my_none?() { |n| n > 6 }
 
 puts 'my-count'
-puts my_arr.my_count{ |n| n % 2 == 0 }
+puts my_arr.my_count() { |n| (n % 2).zero? }
 
 puts 'my_map'
-puts my_arr.my_map{ |n| n + 4 }
+puts my_arr.my_map() { |n| n + 4 }
 puts 'my_inject'
-puts my_arr.my_inject(3){ |n, y| n + y}
+puts my_arr.my_inject(3) { |n, y| n + y }
 
 puts 'multiply_els'
-puts my_arr.multiply_els()
+puts my_arr.multiply_els
