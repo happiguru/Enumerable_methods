dd96d421b3dc8ea8f8f4a696ecac7b594083bbda
hguruman <hguruman@gmail.com>
2020-08-18 00:15:27 +0400

    Remove white spacing

--- enumerables.rb
+++ enumerables.rb
@@ -56,7 +56,6 @@ module Enumerable
 
   # My None?
   def my_none?
-
     result = true
     my_each do |item|
       result = false if yield(item)
@@ -102,34 +101,32 @@ end
 my_arr = [34, 1, 25, 91, 6]
 
 puts 'my_each Array'
-(my_arr).my_each do |n|
+[1, 2, 34, 44, 54].my_each do |n|
   puts n
 end
 
 puts 'my_each_with_index Array'
-(my_arr).my_each_with_index do |n, i|
+[55, 6, 89, 74, 57].my_each_with_index do |n, i|
   puts i.to_s + ' : ' + n.to_s
 end
 
-new_arr = [8, 5, 69, 10, 7]
 puts 'my_select'
-puts (new_arr).my_select { |n| n > 6 }
+puts [8, 5, 69, 10, 7].my_select() { |n| n > 6 }
 
 puts 'my_all?'
-puts (my_arr).my_all? { |n| n == 6 }
+puts my_arr.my_all?() { |n| n == 6 }
 
 puts 'my_any?'
-puts (my_arr).my_any? { |n| n == 1 }
+puts my_arr.my_any?() { |n| n == 1 }
 
 puts 'my_none?'
-puts (my_arr).my_none? { |n| n > 6 }
+puts my_arr.my_none?() { |n| n > 6 }
 
 puts 'my-count'
-puts (my_arr).my_count { |n| (n % 2).zero? }
+puts my_arr.my_count() { |n| (n % 2).zero? }
 
 puts 'my_map'
-puts (my_arr).my_map { |n| n + 4 }
-
+puts my_arr.my_map() { |n| n + 4 }
 puts 'my_inject'
 puts my_arr.my_inject(3) { |n, y| n + y }
 
