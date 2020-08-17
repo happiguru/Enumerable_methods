c10ff14b6300aee20fc9e6d29604fb4d56bbdd5c
jurgen1c <jurgen1c@gmail.com>
2020-08-17 11:04:48 -0600

    Fix bugs: linters spacing errors

--- enumerables.rb
+++ enumerables.rb
@@ -1,10 +1,12 @@
 module Enumerable
   # My Each Method
   def my_each
+
     return to_enum(:my_each) unless block_given?
+
     arr = self.class == Array ? self : to_a
     counter = 0
-    while counter <arr.length
+    while counter < arr.length
       yield(arr[counter])
       counter += 1
     end
@@ -12,7 +14,9 @@ module Enumerable
   end
   # My Each With Index
   def my_each_with_index
+
     return to_enum(:my_each_with_index) unless block_given?
+
     arr = self.class == Array ? self : to_a
     counter = 0
     while counter < arr.length
@@ -23,12 +27,15 @@ module Enumerable
   end
   # My Select
   def my_select
+
     new_array = []
-    my_each {|index_value| new_array << index_value if yield(index_value)}
+    my_each { |index_value| new_array << index_value if yield(index_value) }
     puts new_array
   end
+
   # My All?
   def my_all?
+
     result = nil
     my_each do |item|
       if yield(item)
@@ -40,6 +47,7 @@ module Enumerable
   end
   # My Any?
   def my_any?
+
     result = false
     my_each do |item|
       next unless yield(item)
@@ -51,6 +59,7 @@ module Enumerable
   end
   # My None?
   def my_none?
+
     result = false
     my_each do |item|
       if yield(item)
@@ -64,6 +73,7 @@ module Enumerable
   end
   # My Count
   def my_count
+
     counter = 0
     my_each do |item|
       counter += 1 if yield(item)
@@ -72,6 +82,7 @@ module Enumerable
   end
   # My Map 
   def my_map(&block_given)
+
     result = []
     my_each do |element|
       result << block_given.call(element)
@@ -80,6 +91,7 @@ module Enumerable
   end
   # My Inject
   def my_inject(start_value = 0)
+
     counter = 0
     accumulator = start_value
     while counter < self.size
@@ -90,45 +102,45 @@ module Enumerable
   end
   # Multiply LS
   def multiply_els
-    self.my_inject(1){|a, b|a * b}
+
+    self.my_inject(1){ |a, b|a * b  }
   end
-
-  my_arr = [34, 1, 25, 91, 6]
-
-  puts 'my_each Array'
-  [1, 2, 34, 44, 54].my_each do |n|
-    puts n 
-    end
-
-  puts 'my_each_with_index Array'
-  [55, 6, 89, 74, 57].my_each_with_index do |n, i|
-    puts i.to_s + ' : ' + n.to_s
-    end
-
-  puts 'my_select'
-  [8, 5, 69, 10, 7].my_select do |n|
-    n > 6
-  end
-
-  puts 'my_all?'
-  puts my_arr.my_all?{ |n| n == 6 }
-
-  puts 'my_any?'
-  puts my_arr.my_any?{ |n| n == 1 }
-
-  puts 'my_none?'
-  puts my_arr.my_none?{ |n| n > 6 }
-
-  puts 'my-count'
-  puts my_arr.my_count{ |n| n % 2 == 0 }
-
-  puts 'my_map'
-  puts my_arr.my_map{ |n| n + 4 }
-
-  puts 'my_inject'
-  puts my_arr.my_inject(3){ |n, y| n + y}
-
-  puts 'multiply_els'
-  puts my_arr.multiply_els()
-
 end
+
+my_arr = [34, 1, 25, 91, 6]
+
+puts 'my_each Array'
+[1, 2, 34, 44, 54].my_each do |n|
+  puts n 
+  end
+
+puts 'my_each_with_index Array'
+[55, 6, 89, 74, 57].my_each_with_index do |n, i|
+  puts i.to_s + ' : ' + n.to_s
+  end
+
+puts 'my_select'
+[8, 5, 69, 10, 7].my_select do |n|
+  n > 6
+end
+
+puts 'my_all?'
+puts my_arr.my_all?{ |n| n == 6 }
+
+puts 'my_any?'
+puts my_arr.my_any?{ |n| n == 1 }
+
+puts 'my_none?'
+puts my_arr.my_none?{ |n| n > 6 }
+
+puts 'my-count'
+puts my_arr.my_count{ |n| n % 2 == 0 }
+
+puts 'my_map'
+puts my_arr.my_map{ |n| n + 4 }
+
+puts 'my_inject'
+puts my_arr.my_inject(3){ |n, y| n + y}
+
+puts 'multiply_els'
+puts my_arr.multiply_els()
