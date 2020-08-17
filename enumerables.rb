15cb37ae174c19fa4e29ee14c147faeac41cf3b2
hguruman <hguruman@gmail.com>
2020-08-18 00:07:53 +0400

    Add: parenthesis to the array as detected by the linters

--- enumerables.rb
+++ enumerables.rb
@@ -95,39 +95,41 @@ module Enumerable
 
   # Multiply LS
   def multiply_els
-    self.my_inject(1) { |a, b|a * b }
+    self.my_inject(1) { |a, b| a * b }
   end
 end
 
 my_arr = [34, 1, 25, 91, 6]
 
 puts 'my_each Array'
-[1, 2, 34, 44, 54].my_each do |n|
-    puts n
+(my_arr).my_each do |n|
+  puts n
 end
 
 puts 'my_each_with_index Array'
-[55, 6, 89, 74, 57].my_each_with_index do |n, i|
-    puts i.to_s + ' : ' + n.to_s
+(my_arr).my_each_with_index do |n, i|
+  puts i.to_s + ' : ' + n.to_s
 end
 
+new_arr = [8, 5, 69, 10, 7]
 puts 'my_select'
-puts [8, 5, 69, 10, 7].my_select() { |n| n > 6 }
+puts (new_arr).my_select { |n| n > 6 }
 
 puts 'my_all?'
-puts my_arr.my_all?() { |n| n == 6 }
+puts (my_arr).my_all? { |n| n == 6 }
 
 puts 'my_any?'
-puts my_arr.my_any?() { |n| n == 1 }
+puts (my_arr).my_any? { |n| n == 1 }
 
 puts 'my_none?'
-puts my_arr.my_none?() { |n| n > 6 }
+puts (my_arr).my_none? { |n| n > 6 }
 
 puts 'my-count'
-puts my_arr.my_count() { |n| (n % 2).zero? }
+puts (my_arr).my_count { |n| (n % 2).zero? }
 
 puts 'my_map'
-puts my_arr.my_map() { |n| n + 4 }
+puts (my_arr).my_map { |n| n + 4 }
+
 puts 'my_inject'
 puts my_arr.my_inject(3) { |n, y| n + y }
 
