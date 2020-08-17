c37bc4cf93873658d44012c1f599351c8ee09c44
hguruman <hguruman@gmail.com>
2020-08-17 22:01:11 +0400

    Fix bug: Added missing end of my_count function

--- enumerables.rb
+++ enumerables.rb
@@ -1,6 +1,6 @@
 module Enumerable
    # My Each Method
-   def my_each
+  def my_each
     return to_enum(:my_each) unless block_given?
 
     arr = self.class == Array ? self : to_a
@@ -75,10 +75,12 @@ module Enumerable
   # My Count
   def my_count
     counter = 0
-    my_each do |item|
-      counter += 1 if yield(item)
-    end
+    my_each do |item| 
+      if yield(item)
+        counter += 1
+      end
     counter
+    end
   end
 
   # My Map 
@@ -106,3 +108,41 @@ module Enumerable
     self.my_inject(1){ |a, b|a * b  }
   end
 end
+
+my_arr = [34, 1, 25, 91, 6]
+
+puts 'my_each Array'
+[1, 2, 34, 44, 54].my_each do |n|
+  puts n 
+  end
+
+puts 'my_each_with_index Array'
+[55, 6, 89, 74, 57].my_each_with_index do |n, i|
+  puts i.to_s + ' : ' + n.to_s
+  end
+
+puts 'my_select'
+[8, 5, 69, 10, 7].my_select do |n|
+  n > 6
+end
+
+puts 'my_all?'
+puts my_arr.my_all?{ |n| n == 6 }
+
+puts 'my_any?'
+puts my_arr.my_any?{ |n| n == 1 }
+
+puts 'my_none?'
+puts my_arr.my_none?{ |n| n > 6 }
+
+puts 'my-count'
+puts my_arr.my_count{ |n| n % 2 == 0 }
+
+puts 'my_map'
+puts my_arr.my_map{ |n| n + 4 }
+
+puts 'my_inject'
+puts my_arr.my_inject(3){ |n, y| n + y}
+
+puts 'multiply_els'
+puts my_arr.multiply_els()
