08185217cbf2d88e29cded05a9f6cf8c5d56d4d6
jurgen1c <jurgen1c@gmail.com>
2020-08-17 13:16:18 -0600

    Fix end of input error on my_any? method

--- enumerables.rb
+++ enumerables.rb
@@ -28,20 +28,18 @@ module Enumerable
   # My Select
   def my_select
     new_array = []
-    my_each { |index_value| new_array << index_value if yield(index_value) }
-      puts new_array
+    my_each do |index_value| 
+      new_array << index_value if yield(index_value)
     end
+    new_array
   end
 
   # My All?
   def my_all?
-    result = nil
+    result = false
     my_each do |item|
-    if yield(item == true)
-        result = true
-      else
-        result = false
-      end
+      result = true if yield(item == true)       
+    end
     result
   end
 
@@ -51,9 +49,8 @@ module Enumerable
     result = false
     my_each do |item|
       next unless yield(item)
-        result = true
-      end
-      result
+
+      result = true
     end
     result
   end
@@ -61,14 +58,9 @@ module Enumerable
   # My None?
   def my_none?
 
-    result = false
+    result = true
     my_each do |item|
-      if yield(item)
-        result = false
-      else
-        result = true
-      end
-      result
+      result = false if yield(item)
     end
     result
   end
@@ -76,8 +68,8 @@ module Enumerable
   # My Count
   def my_count
     counter = 0
-    my_each do |item| if yield(item)
-      counter += 1
+    my_each do |item| 
+      counter += 1 if yield(item)
     end
     counter
   end
@@ -104,6 +96,41 @@ module Enumerable
 
   # Multiply LS
   def multiply_els
-    self.my_inject(1){ |a, b|a * b  }
+    self.my_inject(1){ |a, b|a * b }
   end
 end
+
+my_arr = [34, 1, 25, 91, 6]
+
+puts 'my_each Array'
+[1, 2, 34, 44, 54].my_each do |n|
+  puts n 
+  end
+
+puts 'my_each_with_index Array'
+[55, 6, 89, 74, 57].my_each_with_index do |n, i|
+  puts i.to_s + ' : ' + n.to_s
+  end
+
+puts 'my_select'
+puts [8, 5, 69, 10, 7].my_select { |n| n > 6 }
+
+puts 'my_all?'
+puts my_arr.my_all?{ |n| n == 6 }
+
+puts 'my_any?'
+puts my_arr.my_any?{ |n| n == 1 }
+
+puts 'my_none?'
+puts my_arr.my_none?{ |n| n > 6 }
+
+puts 'my-count'
+puts my_arr.my_count{ |n| n % 2 == 0 }
+
+puts 'my_map'
+puts my_arr.my_map{ |n| n + 4 }
+puts 'my_inject'
+puts my_arr.my_inject(3){ |n, y| n + y}
+
+puts 'multiply_els'
+puts my_arr.multiply_els()
