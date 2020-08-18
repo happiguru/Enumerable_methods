3ca099f8f56a3b5b5689bcfd508009a7c97f34f1
hguruman <hguruman@gmail.com>
2020-08-19 01:34:40 +0400

    Updated the code with test examples

--- enumerables.rb
+++ enumerables.rb
@@ -177,3 +177,27 @@ puts Range.new(0, 9).my_all? {|n| n != 6}
 
 new_harsh = { a: 1, b: 2, c: 3, d: 4, e: 5 }
 puts new_harsh.my_all? {|n, y| y == 6}
+
+puts 'my_none?'
+puts (0..9).my_none?{|n| n < 10}
+
+puts Range.new(0, 9).my_none? {|n| n != 6}
+
+new_harsh = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+puts new_harsh.my_none? {|n, y| y == 6}
+
+puts 'my_any?'
+puts (0..9).my_any?{|n| n < 10}
+
+puts Range.new(0, 9).my_any? {|n| n != 6}
+
+new_harsh = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+puts new_harsh.my_any? {|n, y| y == 6}
+
+puts 'my_count'
+puts (0..9).my_count{|n| n < 10}
+
+puts Range.new(0, 9).my_count {|n| n != 6}
+
+new_harsh = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+puts new_harsh.my_count {|n, y| y == 6}
