1b769e0cde875e3cb132557e830a2a9b72e1005e
hguruman <hguruman@gmail.com>
2020-08-19 03:31:59 +0400

    Fix bug: removed whitespaces and added misssing end tag

--- enumerables.rb
+++ enumerables.rb
@@ -1,5 +1,3 @@
-# rubocop:disable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity,Metrics/ModuleLength
-
 module Enumerable
   # My Each Method
   def my_each
@@ -28,7 +26,6 @@ module Enumerable
     new_array = []
     Array(self).my_each do |index_value|
       next unless yield(index_value)
-
       new_array.push(index_value)
     end
     new_array
@@ -37,16 +34,19 @@ module Enumerable
   # My All?
   def my_all?(argument = nil)
     Array(self).my_each do |item|
+    
       if !block_given?
+      
         case argument
-        when argument.nil?
-          next if item
-        when argument.class == Class
-          next if item.is_a? argument
-        when argument.class == regexp
-          next if item =~ argument
-        when (item.is_a? Numeric) || (item.is_a? String)
-          next if item == argument
+        
+          when argument.nil?
+            next if item
+          when argument.class==Class
+            next if item.is_a? argument
+          when argument.class == regexp
+            next if item =~ argument
+          when (item.is_a?Numeric) || (item.is_a? String)
+            next if item == argument
         end
       elsif yield(item)
         next
@@ -62,16 +62,16 @@ module Enumerable
       if !block_given?
         case arr
 
-        when arr.nil?
-          next unless arg
-        when arr.class == Class
-          next unless arg.is_a? arr
-        when arr.class == Regexp
-          next unless arg =~ arr
-        when (arg.is_a? Numeric) || (arg.is_a? String)
-          next unless arg == arr
+          when arr.nil?
+            next unless arg
+          when arr.class == Class
+            next unless arg.is_a? arr
+          when arr.class == Regexp
+            next unless arg =~ arr
+          when (arg.is_a? Numeric) || (arg.is_a? String)
+            next unless arg == arr
         end
-      else
+      else 
         next unless yield(arg)
       end
       return true
@@ -85,14 +85,14 @@ module Enumerable
       if !block_given?
         case arr
 
-        when arr.nil?
-          next unless arg
-        when arr.class == Class
-          next unless arg
-        when arr.class == Regexp
-          next unless arg =~ arr
-        when (arg.is_a? Numeric) || (arg.is_a? String)
-          next unless arg == arr
+          when arr.nil?
+            next unless arg
+          when arr.class == Class
+            next unless arg
+          when arr.class == Regexp
+            next unless arg =~ arr
+          when (arg.is_a? Numeric) || (arg.is_a? String)
+            next unless arg == arr
         end
       else
         next unless yield(arg)
@@ -139,33 +139,75 @@ module Enumerable
   end
 
   # My Inject
-  def my_inject(*arr)
-    raise('LocalJumpError.new NO BLOCK OR ARGUMENT GIVEN!') if !block_given? && arguments.empty?
-    skip_flag = false
+  def my_inject(*argument)
+    flag = false
     accumulator = Array(self)[0]
-    if (arr[0].class == Symbol) || arr[0].nil?
-      skip_flag = true
-    elsif arr[0].is_a? Numeric
-      accumulator = arr[0]
-    end
-    Array(self).my_each_with_index do |arg, i|
-      next if skip_flag && i.zero?
-
-      if block_given?
-        accumulator = yield(accumulator, arg)
-      elsif arr[0].class == Symbol
-        accumulator = accumulator.send(arr[0], arg)
-      elsif arr[0].is_a? Numeric
-        accumulator = accumulator.send(arr[1], arg)
+    if (argument[0].class = Symbol) || argument
+      while counter < arr.size
+        accumulator = yield(accumulator, arr[counter])
+        counter += 1
       end
-    end
     accumulator
+    end
+  end
+
+  # Multiply LS
+  def multiply_els(arr)
+    arr.my_inject(:*)
   end
 end
 
-# rubocop:enable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity,Metrics/ModuleLength
-
-# Multiply LS
-def multiply_els(arr)
-  arr.my_inject(:*)
+puts 'my_each'
+(0..9).my_each do |n|
+  puts n
 end
+
+Range.new(0, 9).my_each do |n|
+  puts n
+end
+
+puts 'my_each_with_index'
+(0..9).my_each_with_index do |n, i|
+  puts i.to_s + ' : ' + n.to_s
+end
+
+Range.new(0, 9).my_each_with_index do |n, i|
+    puts i.to_s + ' : ' + n.to_s
+end
+
+puts 'my_select'
+puts (0..9).my_select { |n| n < 6 }
+
+puts Range.new(0, 9).my_select { |n| n > 6 }
+
+puts 'my_all?'
+puts (0..9).my_all?{|n| n < 10}
+
+puts Range.new(0, 9).my_all? {|n| n != 6}
+
+new_harsh = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+puts new_harsh.my_all? {|n, y| y == 6}
+
+puts 'my_none?'
+puts (0..9).my_none?{|n| n < 10}
+
+puts Range.new(0, 9).my_none? {|n| n != 6}
+
+new_harsh = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+puts new_harsh.my_none? {|n, y| y == 6}
+
+puts 'my_any?'
+puts (0..9).my_any?{|n| n < 10}
+
+puts Range.new(0, 9).my_any? {|n| n != 6}
+
+new_harsh = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+puts new_harsh.my_any? {|n, y| y == 6}
+
+puts 'my_count'
+puts (0..9).my_count{|n| n < 10}
+
+puts Range.new(0, 9).my_count {|n| n != 6}
+
+new_harsh = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+puts new_harsh.my_count {|n, y| y == 6}
