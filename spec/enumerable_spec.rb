6f3ea5c2c06b2e3ab01fc3b088d70b382ecb89ca
hguruman <hguruman@gmail.com>
2020-08-29 01:05:52 +0400

    Added spec for my_each and my_each_with_index method

---
+++ spec/enumerable_spec.rb
@@ -0,0 +1,16 @@
+require "../enumerables.rb"
+test_array1 = [11, 2, 3, 56]
+describe Enumerable do
+    describe "#my_each" do
+        it "returns the input array" do
+            expect(test_array1.my_each { |x| print x }).to eql(test_array1)
+        end
+    end
+
+    describe "#my_each_with_index" do
+        it "returns the array and it index" do
+            expect(test_array1.my_each_with_index { |x, y| }).to eql(test_array1)
+        end
+    end
+end
+
